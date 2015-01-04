73f404a8f363d6a4650199c011023a96c4dcad3a
Charles Butler <chuck@dasroot.net>
2015-01-04 03:20:53 -0500

    Updating custom js with fitvid and github activity

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,1 +1,13 @@
-// Add here all your JS customizations
+/* custom.js - Grab Bag of useful functions to make the site perform as we expect. */
+
+// Make all video's fit their parent wrapper
+$(document).ready(function(){
+    // Target your .container, .wrapper, .post, etc.
+    $(".container").fitVids();
+});
+
+GitHubActivity.feed({
+        username: "chuckbutler",
+        selector: "#github-feed",
+        limit: 2 // optional
+});
